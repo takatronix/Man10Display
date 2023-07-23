5e83dff97071c0694ef6f2b84e874be2f82907ab
takatronix <takatronix@gmail.com>
2023-07-24 05:28:54 +0900

    Update MacroEngine.kt

--- src/main/java/red/man10/display/MacroEngine.kt
+++ src/main/java/red/man10/display/MacroEngine.kt
@@ -8,16 +8,22 @@ import kotlin.math.roundToLong
 enum class CommandType {
     LABEL,
     GOTO,
-    ASSIGNMENT,
+    SET,
     PRINT,
     WAIT,
     IF,
+    LOOP,
+    CLEAR,
+    IMAGE,
+    STRETCH_IMAGE,
 }
 
 data class MacroCommand(
     val type: CommandType,
     val params: List<String>
 )
+
+const val MACRO_SLEEP_TIME = 1L
 
 class MacroEngine {
     private val symbolTable = mutableMapOf<String, Any>()
@@ -27,6 +33,20 @@ class MacroEngine {
     private var currentLineIndex = 0
     private var shouldStop = false
     private var callback: ((MacroCommand, Int) -> Unit)? = null
+    // プロパティとしての変数代入をサポートする
+    private var variableName: String? = null
+    private var isPaused = false
+
+    // マクロを一時停止するメソッド
+    fun pause() {
+        isPaused = true
+    }
+
+    // マクロを再開するメソッド
+    fun resume() {
+        isPaused = false
+    }
+
 
     fun execute(commands: List<MacroCommand>, callback: (MacroCommand, Int) -> Unit) {
         this.commands = commands
@@ -37,12 +57,19 @@ class MacroEngine {
 
         while (currentLineIndex < commands.size && !this.shouldStop) {
             val command = commands[currentLineIndex]
+
+            while (isPaused) {
+                if (shouldStop) {
+                    return
+                }
+                Thread.sleep(MACRO_SLEEP_TIME)
+            }
             when (command.type) {
                 GOTO -> {
                     val label = command.params[0]
                     currentLineIndex = labelIndices[label] ?: throw IllegalArgumentException("Label not found: $label")
                 }
-                ASSIGNMENT -> {
+                SET -> {
                     val variableName = command.params[0].removePrefix("$")
                     val expression = command.params[1]
                     symbolTable[variableName] = evaluateExpression(expression)
@@ -68,6 +95,19 @@ class MacroEngine {
                         currentLineIndex++
                     }
                 }
+                LOOP -> {
+                    // LOOP コマンドの処理
+                    val loopCount = evaluateExpression(command.params[0]) as Int
+                    val loopCommands = commands.subList(currentLineIndex + 1, currentLineIndex + 1 + loopCount)
+                    // 指定された回数だけループする
+                    repeat(loopCount) {
+                        // LOOP 内のコマンドを実行
+                        execute(loopCommands, callback)
+                    }
+                    // ループの終了後、次のコマンドに進む
+                    currentLineIndex += loopCommands.size + 1
+                }
+
                 else -> {
                     // Handle other command types if needed
                     callback(command, currentLineIndex)
@@ -109,12 +149,19 @@ class MacroEngine {
     fun shouldStop(): Boolean {
         return shouldStop
     }
+    // プロパティとしての変数代入をサポートする
+    fun setVariable(variableName: String) {
+        this.variableName = variableName
+    }
     private fun executeCommand(command: MacroCommand) {
         when (command.type) {
-            ASSIGNMENT -> {
-                val variableName = command.params[0].removePrefix("$")
-                val expression = command.params[1]
-                symbolTable[variableName] = evaluateExpression(expression)
+            SET -> {
+                // プロパティとしての変数代入をサポート
+                variableName?.let { varName ->
+                    val expression = command.params[0]
+                    symbolTable[varName] = evaluateExpression(expression)
+                    this.variableName = null // 変数代入が終わったらプロパティをリセット
+                }
             }
             PRINT -> {
                 val expression = command.params[0]
@@ -156,7 +203,7 @@ class MacroEngine {
                 future.cancel(true)
                 return false
             }
-            Thread.sleep(1)
+            Thread.sleep(MACRO_SLEEP_TIME)
         }
         return true
     }
@@ -199,7 +246,12 @@ class MacroEngine {
     }
 
     // 行を解析してMacroCommandに変換する関数
-    fun parseCommand(line: String): MacroCommand? {
+    private fun parseCommand(line: String): MacroCommand? {
+        val trimmedLine = line.trim()
+        if (trimmedLine.isEmpty() || trimmedLine.startsWith("#")) {
+            // 空行またはコメント行の場合はnullを返す
+            return null
+        }
         val parts = line.trim().split(" ")
         if (parts.isEmpty()) {
             return null
@@ -208,10 +260,13 @@ class MacroEngine {
         val type = when (parts[0].toUpperCase()) {
             "LABEL" -> LABEL
             "GOTO" -> GOTO
-            "ASSIGNMENT" -> ASSIGNMENT
+            "SET" -> SET
             "PRINT" -> PRINT
             "WAIT" -> WAIT
             "IF" -> IF
+            "CLEAR" -> CLEAR
+            "IMAGE" -> IMAGE
+            "STRETCH_IMAGE" -> STRETCH_IMAGE
             else -> return null // 不明なコマンドは無視する
         }
 
